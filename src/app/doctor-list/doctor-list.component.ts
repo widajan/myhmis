8313bd92a7d75b05e863fd47cf276941bdd07052
widajan <50039961+widajan@users.noreply.github.com>
2019-07-22 17:08:39 +0430

    Pagination added

--- src/app/doctor-list/doctor-list.component.ts
+++ src/app/doctor-list/doctor-list.component.ts
@@ -1,32 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { DoctorService } from '../doctor.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-doctor-list',
   templateUrl: './doctor-list.component.html',
-  styleUrls: ['./doctor-list.component.scss']
+  styleUrls: ['./doctor-list.component.scss', '../shared/css/style.scss']
 })
 export class DoctorListComponent implements OnInit {
  
   doctors:[];
-  recordPerPage= 2;
+  recPerPage: number = 2;
+  page: number = 1;
+  skip: number = 0;
+  totalPages: number;
+  countDoctors: any;
 
-  constructor( private doctorService: DoctorService ) { 
-    
-}
 
-  ngOnInit() {
-    this.getDoctorList();
+  constructor( 
+    private doctorService: DoctorService,
+    private route: ActivatedRoute ) { }
+
+  async ngOnInit() {
+    this.countDoctors = await this.countAllDoctors();
+    // console.log(this.countDoctors, "alsjfdlkajskl");
+    this.getDoctorList(this.skip);
   }
 
+  getDoctorList(offset) {
+    try {
+        this.skip = offset;
+        this.doctorService.getDoctors(this.skip, this.recPerpage)
+        .subscribe(result => {
+          this.doctors = result;
+          console.log(result); 
+        }, error => {
+          console.log('Error: ', error);
+        })
+      } catch (error) {
+      
+    } 
+  }
 
-  getDoctorList() {
-    this.doctorService.getDoctors()
+  countAllDoctors(): Promise<any>{
+    return new Promise((resolve, reject) => {
+      this.doctorService.countDoctors()
       .subscribe(result => {
-        this.doctors = result
-       
-      }, error => {
-        console.log('Error: ', error);
+        resolve(result);
+      }, err => {
+        reject(err);
       })
+    })
   }
+
 }
--- src/app/doctor.service.ts
+++ src/app/doctor.service.ts
@@ -6,27 +6,33 @@ import Doctor from './doctor';
 
 @Injectable({
   providedIn: 'root'
-})
+}) 
 export class DoctorService {
 
   constructor(
     private http: HttpClient
 ) { }
 
-  getDoctors():  Observable<any> {
-    return this.http.get('/api/doctor/list');
+  getDoctors(skip: number, limit: number):  Observable<any> {
+    let query = `skip=${skip}&limit=${limit}`;
+    return this.http.get(`api/doctor/list?${query}`);
   }
 
   doctorAddData(doctorData: Doctor){
     return this.http.post('api/doctor/add', doctorData);
   }
 
+  // for dropdown add
   getHospital(): Observable<any> {
     return this.http.get('api/hospital/list');
   }
-
+  // for dropdown add
   getDepartment(): Observable<any> {
     return this.http.get('api/department/list');
+  }
+
+  countDoctors(): Observable<any> {
+    return this.http.get("api/doctor/count");
   }
 
 }
